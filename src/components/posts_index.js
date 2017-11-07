29ca19e27026435ff5fa6f92bbfb688e0605cf12
Ofer <oferstemp@gmail.com>
2017-11-07 09:35:13 +0200

    126

---
+++ src/components/posts_index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by ofer on 07/11/17.
+ */
+import React, { Component } from 'react';
+
+class  PostsIndex extends Component{
+    render(){
+        return(
+        <div>
+           PostsIndex
+        </div>
+    );
+    }
+}
+export default PostsIndex;
