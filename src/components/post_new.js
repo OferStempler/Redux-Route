e4b46f9cb64e77cca4ffa91bd56db2a6b1963892
Ofer <oferstemp@gmail.com>
2017-11-13 16:32:52 +0200

    135

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -3,16 +3,59 @@
  */
 import React from 'react';
 import { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component{
 
+     renderTitleField(field) {
+    return (
+        <div className="form-group">
+            <label>Title</label>
+            <input
+                className="form-control"
+                type="text"
+                {...field.input}
+            />
+        </div>
+    );
+     }
+
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+            </div>
+        );
+    }
+
     render(){
         return (
-    <div>
-        NEW POST!
-    </div>
+   <form>
+       <Field
+           label="Title"
+           name="title"
+              component={this.renderField}
+       />
+       <Field
+           label="Tags"
+           name="tags"
+              component={this.renderField}
+       />
+       <Field
+           label="Post Content"
+           name="content"
+           component={this.renderField}
+       />
+   </form>
         );
     }
 }
 
-export default PostsNew;
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
